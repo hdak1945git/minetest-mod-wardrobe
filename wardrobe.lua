16dd98e7e7e19c90f237c949c87d5a29bca572dc
David E <hdak1945git@users.noreply.github.com>
2017-09-05 00:29:14 -0400

    Changed UI to use a list for skin selection
    Pages of buttons are a bit cumbersome once it exceeds more than a two pages;
    as the mouse position is reset whenever a new page is shown.
    
    * Minor code formatting changes (tab alignment).

--- wardrobe.lua
+++ wardrobe.lua
@@ -1,61 +1,51 @@
 local FORM_NAME = "wardrobe_wardrobeSkinForm";
-local SKINS_PER_PAGE = 8;
+local LAST_SELECTION = -1
 
+local function showForm(player)
+    local playerName = player:get_player_name()
+    if not playerName or playerName == "" then return end
 
-local function showForm(player, page)
-   local playerName = player:get_player_name();
-   if not playerName or playerName == "" then return; end
+    local total_skins = #wardrobe.skins
+    if total_skins <= 0 then return end
 
-   local n = #wardrobe.skins;
-   if n <= 0 then return; end
-   local nPages = math.ceil(n/SKINS_PER_PAGE);
+    local fs = "size[5,10]"
+    fs = fs.."label[0,0;Change Into:]"
+    fs = fs.."textlist[0,1;5,8;wardrobe;"
+    for i = 1, total_skins do
+        local skin = wardrobe.skins[i]
+        local skinName =
+            minetest.formspec_escape(wardrobe.skinNames[skin])
+            :gsub("#", "##")
+        fs = fs..skinName
+        if i < total_skins then fs = fs.."," end
+    end
+    fs = fs.."]"
+    fs = fs.."button_exit[0,9;2,2;cancel;Cancel]"
+    fs = fs.."button_exit[3,9;2,2;apply;Apply Skin]"
 
-   if not page or page > nPages then page = 1; end
-   local s = 1 + SKINS_PER_PAGE*(page-1);
-   local e = math.min(s+SKINS_PER_PAGE-1, n);
-
-   local fs = "size[5,10]";
-   fs = fs.."label[0,0;Change Into:]";
-   for i = s, e do
-      local slot = i-s+1;
-      local skin = wardrobe.skins[i];
-      local skinName = minetest.formspec_escape(wardrobe.skinNames[skin]);
-      fs = fs.."button_exit[0,"..slot..";5,1;s:"..skin..";"..skinName.."]";
-   end
-   fs = fs.."label[2,9;Page "..page.."/"..nPages.."]";
-   if page > 1 then
-      fs = fs.."button_exit[0,9;1,1;n:p"..(page-1)..";prev]";
-   end
-   if page < nPages then
-      fs = fs.."button_exit[4,9;1,1;n:p"..(page+1)..";next]";
-   end
-
-   minetest.show_formspec(playerName, FORM_NAME, fs);
+    minetest.show_formspec(playerName, FORM_NAME, fs)
 end
 
 
 minetest.register_on_player_receive_fields(
-   function(player, formName, fields)
-      if formName ~= FORM_NAME then return; end
+    function(player, formName, fields)
+        if formName ~= FORM_NAME then return end
 
-      local playerName = player:get_player_name();
-      if not playerName or playerName == "" then return; end
+        local playerName = player:get_player_name()
+        if not playerName or playerName == "" then return end
+        if not fields then return end
 
-      for fieldName in pairs(fields) do
-         if #fieldName > 2 then
-            local action = string.sub(fieldName, 1, 1);
-            local value = string.sub(fieldName, 3);
-
-            if action == "n" then
-               showForm(player, tonumber(string.sub(value, 2)));
-               return;
-            elseif action == "s" then
-               wardrobe.changePlayerSkin(playerName, value);
-               return;
+        if fields.wardrobe then
+            for e, value in string.gmatch(fields.wardrobe, "(%w+):(%d+)") do
+                LAST_SELECTION = tonumber(value)
             end
-         end
-      end
-   end);
+        elseif fields.apply and LAST_SELECTION >= 0 then
+            local skin = wardrobe.skins[LAST_SELECTION]
+            local skinName = wardrobe.skinNames[skin]
+            wardrobe.changePlayerSkin(playerName, skin)
+        end
+    end
+)
 
 
 minetest.register_node(
@@ -75,7 +65,7 @@ minetest.register_node(
       sounds = default.node_sound_wood_defaults(),
       groups = { choppy = 3, oddly_breakable_by_hand = 2, flammable = 3 },
       on_rightclick = function(pos, node, player, itemstack, pointedThing)
-         showForm(player, 1);
+         showForm(player);
       end
    });
 
